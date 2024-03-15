3db5f03d114d7424ba3e057913527fa8d54c6398
甜瓜(melon) <melonpan082@gmail.com>
2024-03-16 00:45:15 +0900

    [add] 入力画像のアスペクト比設定機能の追加

--- ImageAtlaslizer.cs
+++ ImageAtlaslizer.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.IO;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using OpenCvSharp;
 
 public class Config {
+    public float AspectRatioHori { get; set; }
+    public float AspectRatioVert { get; set; }
     public int AtlasImageWidth { get; set; }
     public int AtlasImageHeight { get; set; }
     public int Row { get; set; }
     public int Col { get; set; }
+    public string? ExportFileType { get; set; }
+    public int ExportQuality { get; set; }
 }
 
 public class ImageAtlaslizer {
+    string[] ImageExtensionArray = ["jpeg", "jpg", "png", "webp"];
+
     public static void Main(string[] args) {
         // Error発生時、勝手に落ちない用
         try {
@@ -32,12 +39,16 @@ public class ImageAtlaslizer {
 
     public int AtlaslizeImages(string[] args) {
         // Config情報(Default値)
+        float AspectRatioHori = 1f;
+        float AspectRatioVert = 1.41421356f;
         int AtlasImageWidth = 2048;
         int AtlasImageHeight = 2048;
         int Row = 4;
         int Col = 4;
+        string ExportFileType = "png";
+        int ExportQuality = 5;
 
-        Console.WriteLine("ImageAtlaslizer 0.99b(23/09/21)   Written by melon.");
+        Console.WriteLine("ImageAtlaslizer 1.1.0(23/12/01)   Written by melon.");
         Console.WriteLine("Program Start.");
 
         // Configファイル読み込み
@@ -52,10 +63,14 @@ public class ImageAtlaslizer {
             }
             if (Config != null) {
                 // 値を移し替え
+                AspectRatioHori = Config.AspectRatioHori;
+                AspectRatioVert = Config.AspectRatioVert;
                 AtlasImageWidth = Config.AtlasImageWidth;
                 AtlasImageHeight = Config.AtlasImageHeight;
                 Row = Config.Row;
                 Col = Config.Col;
+                ExportFileType = Config.ExportFileType;
+                ExportQuality = Config.ExportQuality;
             } else {
                 // 読み込み失敗
                 Console.WriteLine("Read Config File: Failed. Use a default value instead.");
@@ -67,6 +82,13 @@ public class ImageAtlaslizer {
 
         // Config値検証
         Boolean ConfigValidation = true;
+
+        if (AspectRatioHori < 0 || AspectRatioVert < 0) {
+            Console.WriteLine("[Error] Aspect Ratio is below 0.\n\tThe value must be Grater than 0.");
+            Console.WriteLine($"\tAspectRatioHori: {AspectRatioHori}, AspectRatioVert: {AspectRatioVert}");
+            ConfigValidation = false;
+        }
+
         if (AtlasImageWidth < 1 || AtlasImageHeight < 1) {
             Console.WriteLine("[Error] Atlas Size is below 1.\n\tThe value must be Grater than or equal to 1.");
             Console.WriteLine($"\tAtlasImageWidth: {AtlasImageWidth}, AtlasImageHeight: {AtlasImageHeight}");
@@ -81,6 +103,24 @@ public class ImageAtlaslizer {
             Console.WriteLine("[Error] There are too many requests for the output image size.\n\tIncrease the Atlas size or reduce the inputs.");
             Console.WriteLine($"\tAtlasImageWidth: {AtlasImageWidth}, Row: {Row}");
             Console.WriteLine($"\tAtlasImageHeight: {AtlasImageHeight}, Col: {Col}");
+            ConfigValidation = false;
+        }
+
+        if (!ImageExtensionArray.Contains(ExportFileType)) {
+            Console.WriteLine($"[Error] ExportFileType is Not Supporting. Support file: {String.Join(", ", ImageExtensionArray)}");
+            Console.WriteLine($"\tExportFiletype: {ExportFileType}");
+            ConfigValidation = false;
+        }
+
+        if (ExportQuality < 0 || 100 < ExportQuality) {
+            Console.WriteLine("[Error] ExportQuality is Out of Range. Enter the value from 0 to 100.");
+            Console.WriteLine($"\tExportQuality: {ExportQuality}");
+            ConfigValidation = false;
+        }
+
+        if(ExportFileType.Equals("png") && 9 < ExportQuality) {
+            Console.WriteLine("[Error] ExportQuality is Out of Range(png). Enter the value from 0 to 9.");
+            Console.WriteLine($"\tExportQuality: {ExportQuality}");
             ConfigValidation = false;
         }
 
@@ -119,7 +159,7 @@ public class ImageAtlaslizer {
         }
 
         // 画像一覧読み込み
-        String[] ImagesPath;
+        List<String> ImagesPath = new List<String>();
         String ImportDir = Directory.GetCurrentDirectory() + "\\Import";
         // フォルダ確認
         if (!Directory.Exists(ImportDir)) {
@@ -129,15 +169,23 @@ public class ImageAtlaslizer {
             Console.WriteLine($"\tDirectory Path: {ImportDir}");
             return -1;
         }
-        /// 画像一覧読み込み
-        ImagesPath = Directory.GetFiles(ImportDir);
+        /// ファイル読み込み
+        String[] FilePath = Directory.GetFiles(ImportDir);
+        /// 拡張子が画像のもののみ抽出
+        String pattern = @"\.(jpg|jpeg|png|bmp)";
+        foreach (String path in FilePath) {
+            String ext = Path.GetExtension(path);
+            if(Regex.IsMatch(ext, pattern)) {
+                ImagesPath.Add(path);
+            }
+        }
 
         // 読み込み&リサイズ
         Mat[] ResizeImages = new Mat[ImageCount];
         for (int i = 0; i < ImageCount; i++) {
             // ファイルが存在するか確認
             Mat Mat = new Mat();
-            if (i < ImagesPath.Length && File.Exists(ImagesPath[i])) {
+            if (i < ImagesPath.Count && File.Exists(ImagesPath[i])) {
                 Console.WriteLine($"Read File: {ImagesPath[i]}");
                 Mat = new Mat(ImagesPath[i]);
             } else {
@@ -146,10 +194,10 @@ public class ImageAtlaslizer {
                 Mat = new Mat(new Size(6, 8), MatType.CV_8UC3, new Scalar(0, 0, 0));
             }
 
-            // 白銀比(1:sqrt(2)) じゃないとき、空部分を埋める
+            // アスペクト比が一致していないとき、空部分を埋める
             /// 理想値算出
-            int ExpectedWidth = (int)Math.Round((double)Mat.Height / Math.Sqrt(2));
-            int ExpectedHeight = (int)Math.Round((double)Mat.Width * Math.Sqrt(2));
+            int ExpectedWidth = (int)Math.Round((double)Mat.Height / AspectRatioVert * AspectRatioHori);
+            int ExpectedHeight = (int)Math.Round((double)Mat.Width / AspectRatioHori *  AspectRatioVert);
             /// 空白を埋める(padding)
             if (ExpectedWidth > Mat.Width) {
                 Console.WriteLine($"Padding(Width): +{(ExpectedWidth - Mat.Width)}px");
@@ -188,7 +236,7 @@ public class ImageAtlaslizer {
 
         // 出力
         String Date = DateTime.Now.ToString("yyyyMMdd_HHmmss");
-        String ResultFileName = "Result_" + Date + "_" + AtlasImageWidth + "x" + AtlasImageHeight + ".png";
+        String ResultFileName = "Result_" + Date + "_" + AtlasImageWidth + "x" + AtlasImageHeight + "." + ExportFileType;
         Console.WriteLine($"Export File: {ResultFileName}");
         String ExportDir = Directory.GetCurrentDirectory() + "\\Export";
         // Exportフォルダがなかったら、新規作成
@@ -197,7 +245,18 @@ public class ImageAtlaslizer {
             Console.WriteLine("[Notice] Export Folder is Not Found. So this Program created the Folder.");
             Console.WriteLine($"\tDirectory Path: {ExportDir}");
         }
-        AtlasImage.SaveImage(ExportDir + "\\" + ResultFileName);
+        // jpegなら
+        if (Regex.IsMatch(ExportFileType, "(jpeg|jpg)")) {
+            AtlasImage.SaveImage(ExportDir + "\\" + ResultFileName, new ImageEncodingParam(ImwriteFlags.JpegQuality, ExportQuality));
+        }
+        // pngなら
+        if (Regex.IsMatch(ExportFileType, "png")) {
+            AtlasImage.SaveImage(ExportDir + "\\" + ResultFileName, new ImageEncodingParam(ImwriteFlags.PngCompression, ExportQuality));
+        }
+        // webpなら
+        if (Regex.IsMatch(ExportFileType, "webp")) {
+            AtlasImage.SaveImage(ExportDir + "\\" + ResultFileName, new ImageEncodingParam(ImwriteFlags.WebPQuality, ExportQuality));
+        }
 
         // 終了
         Console.WriteLine("Finished.");
